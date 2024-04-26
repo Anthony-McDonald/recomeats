f12e6625b9f397f9c2989bf04ed8e6bd9bb2fd9a
Anthony McDonald <89093671+Anthony-McDonald@users.noreply.github.com>
2024-04-26 18:56:09 +0100

    backend urls implemented

--- server/index.js
+++ server/index.js
@@ -212,14 +212,14 @@ app.post("/addrecipe/:user_id", asyncHandler(async(req, res, next) => {
         const ingredient = recipe_ingredients[i];
         const newIngredient = await pool.query("INSERT INTO RecipeIngredients (recipe_id, ingredient_name) VALUES ($1, $2) RETURNING *",
         [newRecipe.rows[0].recipe_id, ingredient]);
-        addedIngredients.push(newIngredient.rows[0]); // Push the inserted ingredient object to addedIngredients array
+        addedIngredients.push(newIngredient.rows[0]); 
     }
 
-    // Construct the response object
-    const response = {
-        recipe: newRecipe.rows[0], // Include details of the newly added recipe
 
-        // Construct the details for each added ingredient
+    const response = {
+        recipe: newRecipe.rows[0], 
+
+
         ingredients: addedIngredients.map(ingredient => {
             return {
                 ingredient_id: ingredient.ingredient_id,
@@ -235,20 +235,84 @@ app.post("/addrecipe/:user_id", asyncHandler(async(req, res, next) => {
 // Delete Recipe From User
 
 app.delete("/deleterecipe/:user_id/:recipe_id", asyncHandler(async(req, res, next) => {
-    
+    const user_id = req.params.user_id;
+    const recipe_id = req.params.recipe_id;
+
+    await pool.query("DELETE FROM RecipeIngredients WHERE recipe_id = $1", [recipe_id]);
+
+    await pool.query("DELETE FROM Recipes WHERE user_id = $1 AND recipe_id = $2", [user_id, recipe_id]);
+
+    res.send();
+
 }))
 
-// Change Recipe Details
+app.post("/changerecipe/:user_id/:recipe_id", asyncHandler(async(req, res, next) => {
+    const user_id = req.params.user_id;
+    const recipe_id = req.params.recipe_id;
+    const { recipe_name, recipe_ingredients, recipe_description } = req.body;
+    const addedIngredients = [];
 
-app.put("/changerecipe/:user_id/recipe_id", asyncHandler(async(req, res, next) => {
-    
-}))
+        await pool.query(
+            "UPDATE Recipes SET user_id = $1, recipe_name = $2, recipe_description = $3 WHERE recipe_id = $4",
+            [user_id, recipe_name, recipe_description, recipe_id]
+        );
+
+        await pool.query("DELETE FROM RecipeIngredients WHERE recipe_id = $1", [recipe_id]);
+
+        // Insert new recipe ingredients
+        for (const ingredient of recipe_ingredients) {
+            const newIngredient = await pool.query(
+                "INSERT INTO RecipeIngredients (recipe_id, ingredient_name) VALUES ($1, $2) RETURNING *",
+                [recipe_id, ingredient]
+            );
+            addedIngredients.push(newIngredient.rows[0]); 
+        }
+
+        const response = {
+            recipe: {
+                recipe_id: recipe_id,
+                user_id: user_id,
+                recipe_name: recipe_name,
+                recipe_description: recipe_description
+            },
+            ingredients: addedIngredients.map(ingredient => {
+                return {
+                    ingredient_id: ingredient.ingredient_id,
+                    ingredient_name: ingredient.ingredient_name
+                };
+            })
+        };
+
+        res.json(response);
+}));
+
+
 
 // Get User Recipes
 
 app.get("/getrecipe/:recipe_id", asyncHandler(async(req, res, next) => {
+    const recipe_id = req.params.recipe_id;
 
-}))
+    const result = await pool.query("SELECT recipe_name, recipe_description FROM recipes WHERE recipe_id = $1", [recipe_id]);
+    const recipe = result.rows[0];
+
+    const response = {
+        recipe_name: recipe.recipe_name,
+        recipe_description: recipe.recipe_description
+    };
+
+    res.json(response);
+}));
+// Get Recipe Ingredients
+
+app.get("/getingredients/:recipe_id", asyncHandler(async(req, res, next) => {
+    const recipe_id = req.params.recipe_id;
+
+    const result = await pool.query("SELECT ingredient_name FROM recipeingredients WHERE recipe_id = $1", [recipe_id]);
+    const ingredients = result.rows.map(row => row.ingredient_name); 
+
+    res.json(ingredients);
+}));
 
 app.listen(5000, () => {
     console.log("Server has started on port 5000")
