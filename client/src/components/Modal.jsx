c01af417da7779360d0c65d29b4f6d0b97d6f12c
Anthony McDonald <89093671+Anthony-McDonald@users.noreply.github.com>
2024-04-29 00:47:46 +0100

    base register box

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import RegisterModal from './RegisterModal';
 
 const Modal = () => {
+  const [showRegister, setShowRegister] = useState(false);
+
+  const switchReg = () => {
+    console.log("switching reg")
+    event.preventDefault();
+    setShowRegister(!showRegister); // Toggle the state of showRegister
+  };
+
   return (
     <>
-      <button type="button" className=" header-button btn btn-primary log-button" data-bs-toggle="modal" data-bs-target="#exampleModal">
+      <button type="button" className="header-button btn btn-primary log-button" data-bs-toggle="modal" data-bs-target="#exampleModal">
         Sign in
       </button>
 
@@ -14,7 +23,7 @@ const Modal = () => {
           <div className="modal-content">
             <div className="modal-header">
               <h5 className="modal-title" id="exampleModalLabel">Sign in</h5>
-              <button type="button" className=" header-button btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              <button type="button" className="header-button btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className="modal-body">
               <form>
@@ -28,20 +37,20 @@ const Modal = () => {
                   <input type="password" className="form-control" id="exampleInputPassword1" value="" />
                 </div>
                 <div className="submit-register">
-                <button type="submit" className="header-button btn btn-primary">Submit</button>
-                <div className="register-div">
-                <a href="#" className="register">Register</a>
+                  <button type="submit" className="header-button btn btn-primary">Submit</button>
+                  <div className="register-div">
+                    <a href="" onClick={switchReg} data-bs-toggle="modal" data-bs-target="#innerModal" className="register">Register</a>
+                  </div>
                 </div>
-                </div>
-
               </form>
             </div>
             <div className="modal-footer">
-              <button type="button" className=" header-button btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" className="header-button btn btn-secondary" data-bs-dismiss="modal">Close</button>
             </div>
           </div>
         </div>
       </div>
+      <RegisterModal></RegisterModal>
     </>
   );
 };
--- client/src/components/RegisterModal.jsx
+++ client/src/components/RegisterModal.jsx
@@ -2,43 +2,46 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-const RegisterModal = ({ isRegisterOpen }) => {
+const RegisterModal = () => {
   return (
     <>
-      {isRegisterOpen ? (
-        <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div className="modal-dialog modal-dialog-centered">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalLabel">Sign in</h5>
-                <button type="button" className="header-button btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div className="modal-body">
-                <form>
-                  <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value="" />
-                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-                  </div>
-                  <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" value="" />
-                  </div>
-                  <div className="submit-register">
-                    <button type="submit" className="header-button btn btn-primary">Submit</button>
-                    <div className="register-div">
-                      <a href="./register" className="register">Register</a>
-                    </div>
-                  </div>
-                </form>
-              </div>
-              <div className="modal-footer">
-                <button type="button" className="header-button btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      ) : null}
+
+<div className="modal fade" id="innerModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div className="modal-dialog-centered modal-dialog">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h5 className="modal-title" id="exampleModalLabel">Register</h5>
+        <button type="button" className="header-button btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div className="modal-body">
+        {/* form start */}
+        <form>
+  <div className="mb-3">
+    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div className="mb-3">
+    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+    <input type="password" className="form-control" id="exampleInputPassword1"></input>
+  </div>
+  <div className="mb-3 form-check">
+    <input type="checkbox" className="form-check-input" id="exampleCheck1"></input>
+    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
+        {/* form end */}
+        
+      </div>
+      <div className="modal-footer">
+        <button type="button" className="header-button btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" className="header-button btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
     </>
   );
 };
