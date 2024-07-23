fe199fda19d2c94102199574b43ea2b1eaa25296
Anthony McDonald <89093671+Anthony-McDonald@users.noreply.github.com>
2024-07-23 21:34:18 +0100

    path to retrieve all posts added

--- server/routes/forumRoutes.js
+++ server/routes/forumRoutes.js
@@ -4,6 +4,8 @@ const asyncHandler = require("express-async-handler");
 const authorisation = require("../middleware/authorise");
 const pool = require("../db");
 
+const { DateTime } = require("luxon");
+
 
 
 
@@ -11,7 +13,7 @@ const pool = require("../db");
 
 router.post("/newthread", authorisation, asyncHandler(async (req, res) => {
     const { title, body, recipe_id, image_store } = req.body;
-    const userId = req.user.id;
+    const user_id = req.user.id;
     let newImageId = null
 
     if (image_store != null) {
@@ -21,7 +23,7 @@ router.post("/newthread", authorisation, asyncHandler(async (req, res) => {
     }
 
     const newPost = await pool.query("INSERT INTO Posts (user_id, recipe_id, title, body, image_id) VALUES ($1,$2,$3,$4,$5) RETURNING *",
-    [userId, recipe_id, title, body, newImageId ]);
+    [user_id, recipe_id, title, body, newImageId ]);
 
 
     const response = {
@@ -32,6 +34,39 @@ router.post("/newthread", authorisation, asyncHandler(async (req, res) => {
     res.json(response);
 }));
 
+// Delete thread
+
+router.delete("/deletethread", authorisation, asyncHandler(async (req, res) => {
+    const user_id = req.user.id;
+    const { post_id } = req.body;
+    const row_to_delete = await pool.query("SELECT user_id, image_id FROM Posts WHERE post_id = $1",[post_id])
+    
+    const rtd_image_id = row_to_delete.rows[0].image_id
+    const rtd_user_id = row_to_delete.rows[0].user_id
+
+    if (user_id === rtd_user_id) {
+        await pool.query("DELETE FROM Posts WHERE post_id = $1",[post_id])
+        await pool.query("DELETE FROM Images WHERE image_id = $1",[rtd_image_id])
+    }
+    res.send()
+}));
+
+
+// Get Threads
+
+router.get("/threadlist", authorisation, asyncHandler(async (req, res) => {
+    const posts = await pool.query("SELECT * FROM posts")
+    const response = posts.rows.map(post => ({
+        title: post.title,
+        body: post.body,
+        image_id: post.image_id,
+        upvotes: post.upvotes,
+        created_at: DateTime.fromJSDate(post.created_at).toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS),
+    }));
+
+    // Send the response
+    res.json(response);
+}));
 
 // Create new comment
 
