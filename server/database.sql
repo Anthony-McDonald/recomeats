2ca4d8bff1920ae6deb81254efb5447cf73e9fd8
Anthony McDonald <89093671+Anthony-McDonald@users.noreply.github.com>
2024-07-23 20:57:31 +0100

    database changes to reflect forum addition as well as changes to existing table declarations to make information present more rigid

--- server/database.sql
+++ server/database.sql
@@ -1,14 +1,14 @@
 CREATE TABLE Users (
     user_id SERIAL PRIMARY KEY,
-    user_name VARCHAR(50),
-    email_address VARCHAR(50) UNIQUE,
-    password_hash VARCHAR(128),
-    password_salt VARCHAR(32)
+    user_name VARCHAR(50) NOT NULL,
+    email_address VARCHAR(50) UNIQUE NOT NULL,
+    password_hash VARCHAR(128) NOT NULL,
+    password_salt VARCHAR(32) NOT NULL
 );
 
 CREATE TABLE UserProfiles (
     profile_id SERIAL PRIMARY KEY,
-    user_id INT UNIQUE,
+    user_id INT UNIQUE NOT NULL,
     first_name VARCHAR(20),
     last_name VARCHAR(20),
     date_of_birth DATE,
@@ -18,28 +18,28 @@ CREATE TABLE UserProfiles (
 
 CREATE TABLE UserPermissions (
     permission_id SERIAL PRIMARY KEY,
-    user_id INT UNIQUE,
-    permission_level INT,
+    user_id INT UNIQUE NOT NULL,
+    permission_level INT NOT NULL DEFAULT 0,
     FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
+CREATE TABLE CuisinePreferences (
+    preference_id SERIAL PRIMARY KEY,
+    preference_name VARCHAR(50) UNIQUE NOT NULL
+);
+
 CREATE TABLE UserCuisinePreferences (
-    user_id INT,
-    preference_id INT,
+    user_id INT NOT NULL,
+    preference_id INT NOT NULL,
     PRIMARY KEY (user_id, preference_id),
     FOREIGN KEY (user_id) REFERENCES Users(user_id),
     FOREIGN KEY (preference_id) REFERENCES CuisinePreferences(preference_id)
 );
 
-CREATE TABLE CuisinePreferences (
-    preference_id SERIAL PRIMARY KEY,
-    preference_name VARCHAR(50) UNIQUE
-);
-
 CREATE TABLE Recipes (
     recipe_id SERIAL PRIMARY KEY,
-    user_id INT,
-    recipe_name VARCHAR(50),
+    user_id INT NOT NULL,
+    recipe_name VARCHAR(50) NOT NULL,
     recipe_description VARCHAR(250),
     recipe_instructions VARCHAR(500),
     FOREIGN KEY (user_id) REFERENCES Users(user_id)
@@ -47,7 +47,49 @@ CREATE TABLE Recipes (
 
 CREATE TABLE RecipeIngredients (
     ingredient_id SERIAL PRIMARY KEY,
-    recipe_id INT,
-    ingredient_name VARCHAR(50),
+    recipe_id INT NOT NULL,
+    ingredient_name VARCHAR(50) NOT NULL,
     FOREIGN KEY (recipe_id) REFERENCES Recipes(recipe_id)
+);
+
+CREATE TABLE Images (
+    image_id SERIAL PRIMARY KEY,
+    image_description VARCHAR(50),
+    image_store VARCHAR NOT NULL
+);
+
+CREATE TABLE Posts (
+    post_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    recipe_id INT NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    body VARCHAR(500),
+    image_id INT,
+    upvotes INT DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (image_id) REFERENCES Images(image_id),
+    FOREIGN KEY (recipe_id) REFERENCES Recipes(recipe_id)
+);
+
+CREATE TABLE Comments (
+    comment_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    post_id INT NOT NULL,
+    body VARCHAR(250) NOT NULL,
+    upvotes INT DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (post_id) REFERENCES Posts(post_id)
+);
+
+CREATE TABLE Replies (
+    reply_id SERIAL PRIMARY KEY,
+    comment_id INT NOT NULL,
+    user_id INT NOT NULL,
+    body VARCHAR(250) NOT NULL,
+    upvotes INT DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (comment_id) REFERENCES Comments(comment_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
 );
